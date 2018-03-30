a2ccda76e1d833e415c7b3acb9762eaad21ac18c
anandsinghs257 <anandsinghs257@gmail.com>
2018-03-31 00:54:15 +0530

    style changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @doctors = Doctor.last(6)
   end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,10 +2,14 @@ class Doctor < ApplicationRecord
   geocoded_by :full_address
   after_validation :geocode
 
-  mount_uploader :profile_image, ImageUploader
+  mount_uploader :profile_photo, ImageUploader
 
   def full_address
     "#{self.address} #{self.city} #{self.state} #{self.country} #{self.zipcode}"
+  end
+
+  def name 
+    "#{self.first_name} #{self.last_name}"
   end
 
 end
